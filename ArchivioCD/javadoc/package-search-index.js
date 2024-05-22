5378f7b23694a1cd88df8cb3ab115f1dc48031a2
andrebellu <bellu.andrea@outlook.it>
2024-05-22 17:13:59 +0200

    added javadoc - added test package info

---
+++ ArchivioCD/javadoc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"archive"},{"l":"it.unibs.fp.mylib"},{"l":"test"}];updateSearchResults();
---
+++ ArchivioCD/test/test/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Package for test classes.
+ */
+package test;
