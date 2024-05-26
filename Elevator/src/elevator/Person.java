993537683413d282e18f71a6daf2a5a14f1e127b
andrebellu <bellu.andrea@outlook.it>
2024-05-26 20:39:10 +0200

    added setter and getters

--- Elevator/src/elevator/Person.java
+++ Elevator/src/elevator/Person.java
@@ -1,11 +1,27 @@
 package elevator;
 
 public class Person {
-    int currentFloor;
-    int destinationFloor;
+    private int currentFloor;
+    private int destinationFloor;
 
     public Person(int currentFloor, int destinationFloor) {
         this.currentFloor = currentFloor;
         this.destinationFloor = destinationFloor;
+    }
+
+    public int getCurrentFloor() {
+        return currentFloor;
+    }
+
+    public void setCurrentFloor(int currentFloor) {
+        this.currentFloor = currentFloor;
+    }
+
+    public void setDestinationFloor(int destinationFloor) {
+        this.destinationFloor = destinationFloor;
+    }
+
+    public int getDestinationFloor() {
+        return destinationFloor;
     }
 }
